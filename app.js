23b20b2d10b0877bb9cb3a51774c5a3278f1d2ba
unknown <rima13.test@gmail.com>
2022-07-12 22:02:54 +0530

    push all files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('/names', (req, res) => {
+  res.send("Rima");
+})
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+})
