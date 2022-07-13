36e008fcc2e2742c263a729c4e7a92c23c6657a4
unknown <rima13.test@gmail.com>
2022-07-13 11:36:52 +0530

    fix code

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
 app.get('/names', (req, res) => {
   res.send("Rima");
 })
